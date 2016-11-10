52100a3029da12f00770087d1c4555da12973ba0
穆鸿翔 <mutou.leo.wood@gmail.com>
2016-11-10 16:06:38 +0800

    函数框架初步完成。

--- IO大训练/源.cpp
+++ IO大训练/源.cpp
@@ -4,6 +4,15 @@
 #include<fstream>//文件流输入输出()
 using namespace std;
 
+template<class T>
+int cppstdo(T out);
+template<class T>
+T cppstdi(T &target);
+template<class T>
+int cppfo(string file,T out);
+template<class T>
+T cppfi(string file,T &target);
+
 int main(void)
 {
 	return 0;
