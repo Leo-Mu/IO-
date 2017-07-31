cde8e56fb3f915defe9dde298debaec23c8cce2f
穆鸿翔 <mutou.leo.wood@gmail.com>
2017-07-31 14:45:06 +0800

    之前居然没有提交吗？

--- IO大训练/源.cpp
+++ IO大训练/源.cpp
@@ -4,16 +4,28 @@
 #include<fstream>//文件流输入输出()
 using namespace std;
 
-template<class T>
-int cppstdo(T out);
-template<class T>
-T cppstdi(T &target);
-template<class T>
-int cppfo(string file,T out);
-template<class T>
-T cppfi(string file,T &target);
+//template<class T>
+//int cppstdo(T out);
+//template<class T>
+//T cppstdi(T &target);
+//template<class T>
+//int cppfo(string file,T out);
+//template<class T>
+//T cppfi(string file,T &target);
 
 int main(void)
 {
+	ifstream in{ "IO.in" };
+	ofstream out("IO.out");
+	int temp;
+	while (in>>temp)
+	{
+		cout << temp << endl;
+		out << temp << " ";
+	}
+	
+	out.close();
+	in.close();
+	system("pause");
 	return 0;
 }
